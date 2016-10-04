ce617ece616f3b73593ebfcc7f011ecbd69d60d7
Ke Wen <kw953413@ohio.edu>
2016-10-04 12:20:30 -0400

    makefile

---
+++ Quiz2.Sample_Student_Project/college.h
@@ -0,0 +1,40 @@
+/**
+*@author Matthew Aberegg
+*@brife Project 3
+*@name CS 2401
+*@date10/8/2015
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "course.h"
+#include "node.h"
+
+
+#ifndef COLLEGE_H
+#define COLLEGE_H
+
+
+class College{
+	public:
+	College(std::string s);
+	~College();
+	College(const College& other);
+	College& operator =(const College& other);
+	void add(course& c);
+	void remove(std::string coursename);
+	void display(std::ostream& outs);
+	double hours();
+	double gpa();
+	void save(std::ostream& outs);
+	void load(std::istream& ins);
+	private:
+	std::string name;
+	node * head;
+
+
+
+
+};
+#endif
